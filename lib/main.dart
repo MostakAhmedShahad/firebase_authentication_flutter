987bbcb8fb9ed6a0f009c784bc2b381cf7f75221
Mostak <mostakahmedshahad74@gmail.com>
2025-03-04 04:02:45 +0600

    connected_file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyAb8rApi3UFwdQqQadDepPbazAW9Mqx50I",
+            appId: "1:1005179153071:web:6226cf83ef6d123739825a",
+            messagingSenderId: "1005179153071",
+            projectId: "flutter-firebase-a72d0"));
+  }
   runApp(const MyApp());
 }
 
