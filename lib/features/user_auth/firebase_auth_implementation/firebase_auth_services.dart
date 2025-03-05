a187c6917bea05e757a752b3cf65965f6105567b
Mostak <mostakahmedshahad74@gmail.com>
2025-03-06 02:20:12 +0600

    error_handling

--- lib/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart
+++ lib/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart
@@ -1,30 +1,39 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_authentication_flutter/global/common/toust.dart';
 
 class FirebaseAuthServices {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<User?> signUpWithEmailAndPassword(String email, String password) async {
+  Future<User?> signUpWithEmailAndPassword(
+      String email, String password) async {
     try {
-      UserCredential credential =
-          await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      UserCredential credential = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
       return credential.user;
     } on FirebaseAuthException catch (e) {
-      print("Firebase Auth Error: ${e.code} - ${e.message}");
-    } catch (e) {
-      print("Unexpected Error: $e");
+      if (e.code == 'email-already-in-use') {
+        showToast(message: "The email already in use");
+      }
+       else {
+        showToast(message: 'An error occurred :${e.code} ');
+      }
     }
     return null;
   }
 
-  Future<User?> signInWithEmailAndPassword(String email, String password) async {
+  Future<User?> signInWithEmailAndPassword(
+      String email, String password) async {
     try {
-      UserCredential credential =
-          await _auth.signInWithEmailAndPassword(email: email, password: password);
+      UserCredential credential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
       return credential.user;
     } on FirebaseAuthException catch (e) {
-      print("Firebase Auth Error: ${e.code} - ${e.message}");
-    } catch (e) {
-      print("Unexpected Error: $e");
+      if (e.code == 'user-not-found' || e.code == 'wrong-password') {
+        showToast(message: "Invalid email or password ");
+      } else {
+        showToast(message: 'An error occurred :${e.code} ');
+      }
+      // print("Firebase Auth Error: ${e.code} - ${e.message}");
     }
     return null;
   }
